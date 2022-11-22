df26a19d1a8b6345dede36e8a4e94ac756b29275
SevenQi <2586172363@qq.com>
2022-11-22 10:55:16 +0800

    .

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import { SessionManager } from "./lib/sessionManager";
 import { SocketClient } from "./lib/socket/client/socketClient";
-import { Socket } from "net";
-import {TcpChannel} from "./lib/socket/client/tcpChannel";
+import { TcpChannel } from "./lib/socket/client/tcpChannel";
 
 
 const sessionManager = new SessionManager();
 
-const sessionId = sessionManager.add(SocketClient,TcpChannel,{address:"192.168.1.34",port:10001})
-sessionManager.onData(sessionId,(message:any)=>{
-       console.log("消息:",message)
+const sessionId = sessionManager.add(SocketClient, TcpChannel, { address: "192.168.1.34", port: 10001 })
+sessionManager.onData(sessionId, (message: any) => {
+  console.log("消息:", message)
 })
-sessionManager.connect(sessionId).then(()=>{
-   setInterval(()=>{
-     sessionManager.send(sessionId, Buffer.from("hello"))
-   },3000)
+sessionManager.connect(sessionId).then(() => {
+  setInterval(() => {
+    sessionManager.send(sessionId, Buffer.from("hello"))
+  }, 3000)
 })
