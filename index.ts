9c39e60977b129a795b27d6567d672deda2bb6c6
SevenQi <2586172363@qq.com>
2022-12-05 16:39:24 +0800

    .

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import {SerialChannel} from "./lib/serialPort/client/serialChannel";
 import {HidChannel} from "./lib/hid/client/hidChannel";
 import {HidClient} from "./lib/hid/client/hidClient";
 import * as HID from "node-hid";
+import {SerialPort} from "serialport";
 // const sessionManager = new SessionManager();
 //
 // const sessionId = sessionManager.add(SocketClient, TcpChannel, { address: "101.32.205.22", port: 8888})
@@ -36,39 +37,44 @@ import * as HID from "node-hid";
 //     })
 // },2000)
 
-// const sessionManager = new SessionManager();
-//
-// const sessionId = sessionManager.add(SerialClient, SerialChannel, {},{
-//   path: '/dev/tty-usbserial1',
-//   baudRate: 9600,
-//   autoOpen: false,
-// })
-// sessionManager.onData(sessionId, (message: any) => {
-//   console.log("消息:", message)
-// })
-// sessionManager.connect(sessionId).then(() => {
-//   setInterval(() => {
-//     sessionManager.send(sessionId, Buffer.from([0x0d,0x0a,0x11,0x11,0x22,0x33]))
-//   }, 3000)
-// })
-// sessionManager.onData(sessionId, (message: any) => {
-//   console.log("消息测试:", message)
-// })
-
 const sessionManager = new SessionManager();
-
-const devices = HID.devices();
-const rfid = devices.filter(x => x.vendorId == 0x03eb && x.productId == 0x2421);
-
-const sessionId = sessionManager.add(HidClient, HidChannel, {}, rfid[0].path)
+// SerialPort.list().then(s=>{
+//     console.log(s)
+// })
+const sessionId = sessionManager.add(SerialClient, SerialChannel, {},{
+  path: '/dev/tty.usbserial-AR0K7IQ6',
+  baudRate: 115200,
+  autoOpen: false,
+})
 sessionManager.onData(sessionId, (message: any) => {
   console.log("消息:", message)
 })
 sessionManager.connect(sessionId).then(() => {
   setInterval(() => {
-    sessionManager.send(sessionId, Buffer.from([0x5A,0x00,0x01,0x02,0xFF,0x00,0x00,0x88,0x5A]))
+
+    sessionManager.send(sessionId, Buffer.from([
+0x01, 0x10, 0x00, 0x01, 0x00, 0x04, 0x08, 0xD5, 0xC5, 0x20, 0x20, 0xC8, 0xFD, 0x20, 0x20, 0x22, 0xF9
+]))
   }, 3000)
 })
 sessionManager.onData(sessionId, (message: any) => {
   console.log("消息测试:", message)
 })
+
+// const sessionManager = new SessionManager();
+//
+// const devices = HID.devices();
+// const rfid = devices.filter(x => x.vendorId == 0x03eb && x.productId == 0x2421);
+//
+// const sessionId = sessionManager.add(HidClient, HidChannel, {}, rfid[0].path)
+// sessionManager.onData(sessionId, (message: any) => {
+//   console.log("消息:", message)
+// })
+// sessionManager.connect(sessionId).then(() => {
+//   setInterval(() => {
+//     sessionManager.send(sessionId, Buffer.from([0x5A,0x00,0x01,0x02,0xFF,0x00,0x00,0x88,0x5A]))
+//   }, 3000)
+// })
+// sessionManager.onData(sessionId, (message: any) => {
+//   console.log("消息测试:", message)
+// })
