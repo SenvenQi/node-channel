e3088caa9a684f8daf0afc9f05f09f7f79eed39f
SevenQi <2586172363@qq.com>
2022-12-13 16:43:11 +0800

    .

--- examples/hid.ts
+++ examples/hid.ts
@@ -7,23 +7,22 @@ const sessionManager = new SessionManager();
 
 const devices = HID.devices();
 const rfid = devices.filter(x => x.vendorId == 0x03eb && x.productId == 0x2421);
-
-const sessionId = sessionManager.add(HidClient, HidChannel, rfid[0].path)
-sessionManager.onData(sessionId, (message: any) => {
-  console.log("消息:", message)
-})
-
 function connect(){
     sessionManager.connect(sessionId).then((result) => {
         if (result)
-                 setInterval(() => {
-    sessionManager.send(sessionId, Buffer.from([0x5A,0x00,0x01,0x02,0xFF,0x00,0x00,0x88,0x5A]))
-  }, 3000)
+            setInterval(() => {
+                sessionManager.send(sessionId, Buffer.from([0x5A,0x00,0x01,0x02,0xFF,0x00,0x00,0x88,0x5A]))
+            }, 3000)
         else
             setTimeout(connect,3000)
     }).catch(e=>{
         setTimeout(connect,3000)
     })
 }
-
+const sessionId = sessionManager.add(HidClient, HidChannel, "/dev/ssss")
+sessionManager.onData(sessionId, (message: any) => {
+    console.log("消息:", message)
+})
 connect();
+
+
