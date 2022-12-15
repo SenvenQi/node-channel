1e8f411724598debbc86ae76e9c86acfebac9190
SevenQi <2586172363@qq.com>
2022-12-15 16:38:36 +0800

    .

--- examples/client.ts
+++ examples/client.ts
@@ -15,9 +15,44 @@ const tcpSessionId = sessionManager.add({
     channelOptions:{
         options:{port:8889,host:"0.0.0.0"},
         filter:new StringFilter()}})
+
+const udpSessionId = sessionManager.add({
+    channelType:ChannelType.Udp,
+    channelOptions:{
+        options:{port:8889,host:"0.0.0.0"},
+        filter:new StringFilter()}})
+
+const serialSessionId = sessionManager.add({
+    channelType:ChannelType.Serial,
+    channelOptions:{
+    options:{
+        path: '/dev/tty.usbserial-AR0K7IQ6',
+        baudRate: 115200,
+        autoOpen: false,
+    },
+    filter:new StringFilter()}})
+
+const hidSessionId = sessionManager.add({
+    channelType:ChannelType.Hid,
+    channelOptions:{
+    options : '/IO/Hid',
+    filter:new StringFilter()}})
+
 sessionManager.onDataAll((message: any) => {
     console.log("消息:", message)
 })
+
+sessionManager.connect(hidSessionId).then(() => {
+    setInterval(() => {
+        sessionManager.send(hidSessionId, "hello hid")
+    }, 3000)
+})
+sessionManager.connect(udpSessionId).then(() => {
+    setInterval(() => {
+        sessionManager.send(udpSessionId, "hello udpSocket")
+    }, 3000)
+})
+
 sessionManager.connect(tcpSessionId).then(() => {
     setInterval(() => {
         sessionManager.send(tcpSessionId, "hello tcpSocket")
@@ -26,5 +61,10 @@ sessionManager.connect(tcpSessionId).then(() => {
 sessionManager.connect(webSocketSessionId).then(() => {
     setInterval(() => {
         sessionManager.send(webSocketSessionId, "hello websocket")
+    }, 3000)
+})
+sessionManager.connect(serialSessionId).then(() => {
+    setInterval(() => {
+        sessionManager.send(serialSessionId, "hello serialPort")
     }, 3000)
 })
